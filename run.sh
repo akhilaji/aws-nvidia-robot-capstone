d1804caf5617429ea1128740fac09ee460710d37
Jacob Cohen <jacobcohen76@protonmail.com>
2020-11-10 14:37:06 -0700

    cleaned up Docker related files

--- run.sh
+++
@@ -1,2 +0,0 @@
-# sudo docker run -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix asu-aws-nvidia-robo
-sudo docker run -i -t --net=host -e DISPLAY -v /tmp/.X11-unix asu-aws-nvidia-robo /bin/bash
---
+++ scripts/midas_wrapper.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+import sys
+
+if __name__ == "__main__":
+    repo_path = os.path.join('workspace','git','MiDaS')
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-i', '--input',
+        default=os.path.join(repo_path, 'input'),
+        help='path to a folder of images, or a single image')
+    arg_parser.add_argument('-o', '--output',
+        default=os.path.join(repo_path, 'output'),
+        help='path to output folder')
+    arg_parser.add_argument('-m', '--model',
+        default=os.path.join('workspace', 'models', 'intel-isl_MiDaS_v2_model-f46da743.pt'),
+        help='path to model')
+    namespace, args = arg_parser.parse_known_args(sys.argv)
+    try:
+        sys.path.append(repo_path)
+        from run import run
+        run(namespace.input, namespace.output, namespace.model)
+    except ImportError as ie:
+        print(ie)
+        exit(1)
