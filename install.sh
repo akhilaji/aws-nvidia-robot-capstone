9044c4d989709546b7e73d59eb19fde31c8e43c8
Akhil <akhilaji99@gmail.com>
2021-02-04 18:23:46 -0800

    added kafka setup and basic streaming code

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+#add and install java
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer -y
+
+#install zookeeper
+sudo apt-get install zookeeperd
+
+#download and install kafka
+wget https://www-eu.apache.org/dist/kafka/2.1.1/kafka_2.11-2.1.1.tgz 
+sudo mkdir /opt/Kafka
+cd /opt/Kafka
+sudo tar -xvf kafka_2.11-2.1.1.tgz -C /opt/Kafka/
