14a643de1cd32093dcf3419fe3cba37d10c6c72a
jwande18 <jwande18@asu.edu>
2021-02-07 18:22:14 -0700

    updated to copy edited darknet sources to darknet

--- install.sh
+++ install.sh
@@ -2,16 +2,22 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-#add and install java
+# add and install java
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer -y
 
-#install zookeeper
+# install zookeeper
 sudo apt-get install zookeeperd
 
-#download and install kafka
+# download and install kafka
 wget https://www-eu.apache.org/dist/kafka/2.1.1/kafka_2.11-2.1.1.tgz 
 sudo mkdir /opt/Kafka
 cd /opt/Kafka
 sudo tar -xvf kafka_2.11-2.1.1.tgz -C /opt/Kafka/
+
+# move darknet source edits into darknet
+cp darknet_edit/demo.c ~/darknet/src/
+cp darknet_edit/image_opencv.cpp ~/darknet/src
+cp darknet_edit/image_opencv.h ~/darknet/src
+
