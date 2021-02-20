9b2de01c2f3dcbd664e450dd242c5769fa2a6350
jwande18 <jwande18@asu.edu>
2021-02-20 16:15:08 -0700

    added statement to print out messages

--- src/integration/kafka/obj_consumer.py
+++ src/integration/kafka/obj_consumer.py
@@ -3,3 +3,11 @@ from kafka import KafkaConsumer
 obj_consumer = KafkaConsumer('vidInput',
                              group_id='camera-group',
                              bootstrap_servers=['localhost:9092'])
+
+for msg in consumer:
+    # message values are in raw bytes
+    # e.g. for unicode: `msg.value.decode('utf-8')`
+
+    print("%s:%d%d: key=%s value=%s" %(msg.topic, msg.partition,
+                                       msg.offset, msg.key,
+                                       msg.value.decode('utf-8')))
