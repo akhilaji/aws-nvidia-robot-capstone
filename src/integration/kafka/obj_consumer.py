0079fdbc8baed5ac51d7beaf9fbc48fd1eeda912
jwande18 <jwande18@asu.edu>
2021-02-21 12:46:40 -0700

    implemented decoding of image byte files

--- src/integration/kafka/obj_consumer.py
+++ src/integration/kafka/obj_consumer.py
@@ -1,10 +1,12 @@
+import cv2
+import numpy as np
 from kafka import KafkaConsumer
 
 obj_consumer = KafkaConsumer('camera-feed',
                              group_id='camera-group',
                              bootstrap_servers=['localhost:9092'])
 
-for msg in consumer:
+for msg in obj_consumer:
     # message values are in raw bytes
     # e.g. for unicode: `msg.value.decode('utf-8')`
 
@@ -12,6 +14,9 @@ for msg in consumer:
                                        msg.offset, msg.key,
                                        msg.value))
 
+    nparr = np.fromstring(msg.value, np.uint8)
+    image_np = cv2.imdecode(nparr, 1)
+
     # save the image
-    img = cv2.imdecode(msg.value, 1)
-    cv2.imwrite("image.jpeg", img)
+    #img = cv2.imdecode(msg.value, 1)
+    cv2.imwrite("image.jpeg", image_np)
