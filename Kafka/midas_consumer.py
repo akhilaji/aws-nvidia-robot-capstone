aeb2e8446493213e6f5bd97161c69f1a5f04bb90
Akhil <akhilaji99@gmail.com>
2021-02-23 18:59:29 -0800

    kafka dual consumer experimentation on the main branch

---
+++ Kafka/midas_consumer.py
@@ -0,0 +1,25 @@
+from flask import Flask, Response
+from kafka import KafkaConsumer
+import numpy as np
+import cv2
+
+
+consumer = KafkaConsumer('vidInput', bootstrap_servers='localhost:9092', group_id='depth-group')
+
+app = Flask(__name__)
+
+
+def kafkastream():
+    for message in consumer:
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + message.value + b'\r\n\r\n')
+
+
+@app.route('/')
+def index():
+    return Response(kafkastream(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run()
