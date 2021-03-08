e2452cdd8cb1d793d1af87d281c5a83a568d651d
jwande18 <jwande18@asu.edu>
2021-03-07 19:29:00 -0700

    reverted dep_consumer to be used without pydarknet Note: see the new branch integration-pydarknet-consumer for python-related development

--- src/integration/kafka/dep_consumer.py
+++ src/integration/kafka/dep_consumer.py
@@ -1,28 +1,21 @@
 import cv2
 import numpy as np
 from kafka import KafkaConsumer
-import depth
-dep_consumer = KafkaConsumer('vidInput',
-                             group_id='depth-group',
+
+dep_consumer = KafkaConsumer('camera-feed',
+                             group_id='camera-group',
                              bootstrap_servers=['localhost:9092'])
 
 for msg in dep_consumer:
-    """
-    The *msg* has the following attributes:
-        - topic: The name of the topic subscribed to.
-    	- partition: The topic partition of the consumer.
-    	- offset: The message offset.
-    	- key: The raw message key.
-    	- value: The raw message value.
-    """
-    
-    # decode message data (bytes -> numpy.array)
-    nparr = np.frombuffer(msg.value, np.uint8)
-    nparr_img = cv2.imdecode(nparr, 1)
+    # message values are in raw bytes
+    # e.g. for unicode: `msg.value.decode('utf-8')`
 
-    # reshape the image array
-    data = cv2.resize(nparr_img, (480,640))
+    print("%s:%d%d: key=%s value=%s" %(msg.topic, msg.partition,
+                                       msg.offset, msg.key,
+                                       msg.value))
 
-    # perform depth detection
-    depth_map = depth.midas_large(data)
-    print(depth_map)
+    # decode message data (bytes -> jpg)
+    nparr = np.fromstring(msg.value, np.uint8)
+    image_np = cv2.imdecode(nparr, 1)
+
+    cv2.imwrite("image.jpg"., image_np)
