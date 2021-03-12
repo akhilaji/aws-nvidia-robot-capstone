668866f67d49ae078578c8048891661fb0e1ec8f
Akhil <akhilaji99@gmail.com>
2021-03-12 15:05:26 -0800

    modified run script

--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@
 
 sudo /opt/Kafka/kafka_2.13-2.7.0/bin/kafka-server-start.sh /opt/Kafka/kafka_2.13-2.7.0/config/server.properties &
 sudo /opt/kafka_2.13-2.7.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1  --partitions 2 --topic vidInput &
+sudo /opt/kafka_2.13-2.7.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1  --partitions 2 --topic objOutput &
+sudo /opt/kafka_2.13-2.7.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1  --partitions 2 --topic depOutput &
 
 #sleep 10s
 #python3 ./src/integration/kafka/cam_producer.py &
