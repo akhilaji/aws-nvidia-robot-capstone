c6bc92659a9dc0e39f5a171cb26f1a306a85841b
jwande18 <jwande18@asu.edu>
2021-03-13 16:12:55 -0700

    adjusted resize to match darknet 608x608

--- src/integration/kafka/dep_consumer.py
+++ src/integration/kafka/dep_consumer.py
@@ -10,22 +10,21 @@ dep_consumer = KafkaConsumer('vidInput',
                              bootstrap_servers=['localhost:9092'])
 
 for msg in dep_consumer:
-    """
+    '''
     The *msg* has the following attributes:
         - topic: The name of the topic subscribed to.
     	- partition: The topic partition of the consumer.
     	- offset: The message offset.
     	- key: The raw message key.
     	- value: The raw message value.
-    """
+    '''
     
     # decode message data (bytes -> numpy.array)
     nparr = np.frombuffer(msg.value, np.uint8)
     nparr_img = cv2.imdecode(nparr, 1)
 
     # reshape the image array
-    data = cv2.resize(nparr_img, (480,640))
+    data = cv2.resize(nparr_img, (608,608))
 
     # perform depth detection
     depth_map = depth.midas_large(data)
-    plt.imsave('image.png', depth_map)
