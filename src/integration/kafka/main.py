a829ce9ebcc84f08d5b2d0708e610c027c386189
Akhil Aji <akhilaji99@gmail.com>
2021-03-18 12:49:29 -0700

    "single file code implementation (UNTESTED)"

---
+++ src/integration/kafka/main.py
@@ -0,0 +1,128 @@
+import os
+import sys
+import datetime
+import cv2
+import numpy as np
+import depth
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from absl import app, flags, logging
+from absl.flags import FLAGS
+import core.utils as utils
+from core.config import cfg
+from core.yolov4 import filter_boxes
+from tensorflow.python.saved_model import tag_constants
+from PIL import Image
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+input_size = 608
+images = FLAGS.images
+
+# load model
+model_path = "./yolov4-608"
+saved_model_loaded = tf.saved_model.load(
+    model_path, tags=[tag_constants.SERVING])
+
+
+def get_depth(data):
+    print("depth function")
+    MidasEstimator = depth.construct_midas_large()
+    depth_map = MidasEstimator(data)
+    return depth_map
+
+
+def get_object(_argv, data):
+    print("object detection function")
+    # detection_properties
+    iou = 0.45
+    score = 0.25
+    flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
+    flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
+    flags.DEFINE_list('images', './data/images/kite.jpg',
+                      'path to input image')
+    flags.DEFINE_string('output', './detections/', 'path to output folder')
+
+    nparr = np.fromstring(data, np.uint8)
+    image_np = cv2.imdecode(nparr, 1)
+
+    original_image = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+
+    image_data = image_np
+    image_data = image_data / 255
+    images_data = []
+    for i in range(1):
+        images_data.append(image_data)
+    images_data = np.asarray(images_data).astype(np.float32)
+
+    infer = saved_model_loaded.signatures['serving_default']
+    batch_data = tf.constant(images_data)
+    pred_bbox = infer(batch_data)
+    for key, value in pred_bbox.items():
+        boxes = value[:, :, 0:4]
+        pred_conf = value[:, :, 4:]
+    boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+        boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+        scores=tf.reshape(
+            pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+        max_output_size_per_class=50,
+        max_total_size=50,
+        iou_threshold=iou,
+        score_threshold=score
+    )
+    pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(),
+                 valid_detections.numpy()]
+
+    # read in all class names from config
+    class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+
+    # by default allow all classes in .names file
+    allowed_classes = list(class_names.values())
+
+    # custom allowed classes (uncomment line below to allow detections for only people)
+    #allowed_classes = ['person']
+
+    image = utils.draw_bbox(original_image, pred_bbox,
+                            allowed_classes=allowed_classes)
+
+    return pred_bbox
+
+
+def main(_argv):
+    print("initializing services")
+
+    # Video Settings
+    # set 0 for camera
+    video_src = 0
+    resolution_width = 608
+    resolution_height = 608
+    dim = (resolution_width, resolution_height)
+
+    video = cv2.VideoCapture(video_src)
+    video.set(cv2.CAP_PROP_AUTOFOCUS, 1)
+
+    while video.isOpened():
+        success, frame = video.read
+        if not success:
+            break
+        frame = cv2.resize(frame, dim)
+
+        data = cv2.imencode('.jpg', frame)
+
+        depth = get_depth(frame)
+        objects = get_object(_argv, data)
+
+        print(depth)
+        print(objects)
+
+
+if __name__ == '__main__':
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
