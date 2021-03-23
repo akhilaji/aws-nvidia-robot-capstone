b8c8fb41dcdf0bd86e5480de952c96a9029c0648
Jacob <jacobcohen76@protonmail.com>
2021-03-23 12:37:28 -0700

    Refactored tracker

--- src/integration/skeleton/track.py
+++ src/integration/skeleton/track.py
@@ -1,20 +1,22 @@
-from skeleton import detect
+from skeleton.detect import BoundingBox, ID, ObjectDetection
+
+import collections
 
 from cv2 import cv2
 import numpy as np
 
-from typing import Any, Callable, Dict, Iterator, List, Mapping, NamedTuple, TypeVar, Set
+from typing import Any, Callable, Dict, Iterator, Iterable, List, Mapping, NamedTuple, Tuple, TypeVar, Set
 from nptyping import NDArray
 
-Centroid = NDArray[(2, 1), np.float32]
+Centroid = NDArray[2, np.float32]
 
-def centroid(bbox: detect.BoundingBox) -> Centroid:
-    return np.array([[bbox.w / 2.0 + bbox.x], [bbox.h / 2.0 + bbox.y]], np.float32)
+def centroid(bbox: BoundingBox) -> Centroid:
+    return np.array([bbox.w / 2.0 + bbox.x, bbox.h / 2.0 + bbox.y], np.float32)
 
 def centroid_distance(c1: Centroid, c2: Centroid) -> float:
-    return (c2[1][0] - c1[1][0]) ** 2.0 + (c2[0][0] - c1[0][0]) ** 2.0
+    return (c2[1] - c1[1]) ** 2.0 + (c2[0] - c1[0]) ** 2.0
 
-def kalman_filter_factory(p_noise_cov_scale: float = 0.03) -> cv2.KalmanFilter:
+def pos_kalman_filter(p_noise_cov_scale: float = 0.03) -> cv2.KalmanFilter:
     k_filter = cv2.KalmanFilter(4, 2)
     k_filter.measurementMatrix  = np.array([[1, 0, 0, 0],
                                             [0, 1, 0, 0]], np.float32)
@@ -35,7 +37,7 @@ class ObjectTracker:
 
     """
 
-    def track(self, detections: List[detect.ObjectDetection]) -> None:
+    def track(self, detections: List[ObjectDetection]) -> None:
         """
 
         """
@@ -43,50 +45,72 @@ class ObjectTracker:
 
 class CentroidTracker:
     class ObjectInstance(NamedTuple):
-        c_filter: cv2.KalmanFilter
-        detection: detect.ObjectDetection
-        age: int
+        kfilter: cv2.KalmanFilter
+        detection: ObjectDetection = None
+        age: int = 0
+
+        def correct(self, measurement: Centroid) -> None:
+            self.kfilter.correct(np.reshape(measurement, (2, 1)))
+        
+        def predict(self) -> Centroid:
+            prediction = self.kfilter.predict()
+            return np.reshape(prediction, (2, 1))
 
     def __init__(self,
-            on_screen: Set[Any] = set(),
-            obj_instance: Dict[Any, ObjectInstance] = dict(),
-            id_itr: Iterator[Any] = iter(int, 1),
+            on_screen: Set[ID] = set(),
+            off_screen: Set[ID] = set(),
+            id_itr: Iterator[ID] = iter(int, 1),
+            dist_f: Callable[[Centroid, Centroid], float] = centroid_distance,
+            kfilter_factory: Callable[[], cv2.KalmanFilter] = pos_kalman_filter,
             pruning_age: int = 50,
             dist_thresh: float = 100.0,
-            filter_factory: Callable[[], cv2.KalmanFilter] = kalman_filter_factory,
-        ):
+        ):        
         self.on_screen = on_screen
-        self.obj_instance = obj_instance
+        self.off_screen = off_screen
         self.id_itr = id_itr
+        self.dist_f = dist_f
+        self.obj_instance = collections.defaultdict(lambda: CentroidTracker.ObjectInstance(kfilter_factory()))
         self.pruning_age = pruning_age
         self.dist_thresh = dist_thresh
-        self.filter_factory = filter_factory
-
-    def __call__(self, detections: List[detect.ObjectDetection]) -> None:
-        on_screen_predictions = [(o_id, self.obj_instance[o_id].c_filter.predict()) for o_id in self.on_screen]
-        enumerated_centroids = [(index, centroid(detection.bbox)) for index, detection in enumerate(detections)]
-        while on_screen_predictions:
-            min_pair, min_dist = (None, None), float('inf')
-            for o_id, o_prediction in on_screen_predictions:
-                for ec_index, ec_centroid in enumerated_centroids:
-                    if detections[ec_index].id == None and \
-                       detections[ec_index].obj_class == self.obj_instance[o_id].detection.obj_class and \
-                       (alt_dist := centroid_distance(o_prediction, ec_centroid)) < min_dist:
+    
+    def __call__(self, detections: List[ObjectDetection]) -> None:
+        c_pred = {o_id: o_inst.predict() for o_id, o_inst in self.obj_instance.items()}        
+        def closest_detection(o_ids: Iterable[ID]) -> Tuple[ID, ObjectDetection]:
+            closest_o_id, closest_det = None, None
+            min_dist = self.dist_thresh
+            for o_id in o_ids:
+                o_id_class = self.obj_instance[o_id].obj_class
+                valid_match = lambda x: x.id == None and x.obj_class == o_id_class
+                for det in filter(valid_match, detections):
+                    alt_dist = self.dist_f(c_pred[o_id], centroid(det.bbox))
+                    if alt_dist < min_dist:
+                        closest_o_id, closest_det = o_id, det
                         min_dist = alt_dist
-                        min_pair = (o_id, ec_index)
-            o_id, ec_index = min_pair
-            if o_id != None and ec_index != None and min_dist < self.dist_thresh:
-                detections[ec_index].id = o_id
-                del on_screen_predictions[o_id]
-        self.on_screen.clear()
-        for detection in detections:
-            if detection.id == None:
-                detection.id = next(self.id_itr)
-                self.obj_instance[detection.id] = CentroidTracker.ObjectInstance(self.filter_factory(), detection, 0)
-            self.obj_instance[detection.id].age = 0
-            self.on_screen.add(detection.id)
-        for o_id, o_instance in self.obj_instance.items():
-            if o_instance.age > self.pruning_age:
+            return closest_o_id, closest_det
+        def assign_id(o_id: ID, det: ObjectDetection) -> None:
+            o_inst = self.obj_instance[o_id]
+            o_inst.correct(centroid(det.bbox))
+            o_inst.detection = det
+            o_inst.age = 0
+            det.id = o_id
+        while self.on_screen:
+            o_id, det = closest_detection(self.on_screen)
+            if o_id == None:
+                break
+            else:
+                assign_id(o_id, det)
+                self.on_screen.remove(o_id)
+        while self.off_screen:
+            o_id, det = closest_detection(self.off_screen)
+            if o_id == None:
+                break
+            else:
+                assign_id(o_id, det)
+                self.off_screen.remove(o_id)
+        for o_id, o_inst in self.obj_instance.items():
+            if o_inst.age > self.pruning_age:
                 del self.obj_instance[o_id]
             else:
-                o_instance.age += 1
+                o_inst.age += 1
+        self.on_screen = {det.id for det in detections}
+        self.off_screen = self.obj_instance.keys() - self.on_screen
