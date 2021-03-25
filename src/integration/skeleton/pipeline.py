8d2e18db6a5d28484e9b797b545d7562820a9a7e
Jacob <jacobcohen76@protonmail.com>
2021-03-25 07:42:08 -0700

    added ConcurrentDetectionPipeline

--- src/integration/skeleton/pipeline.py
+++ src/integration/skeleton/pipeline.py
@@ -4,6 +4,7 @@ from skeleton.pick import PointPicker
 from skeleton.track import ObjectTracker
 
 import numpy as np
+import concurrent.futures
 
 from nptyping import NDArray
 from typing import Any, List
@@ -29,6 +30,33 @@ class DetectionPipeline:
         ) -> List[ObjectDetection]:
         detections = self.object_detector(frame)
         depth_map = self.depth_estimator(frame)
+        self.object_tracker(detections)
+        # self.point_picker(detections, frame, depth_map)
+        return detections
+
+
+class ConcurrentDetectionPipeline(DetectionPipeline):
+
+    def __init__(self,
+            object_detector: ObjectDetector,
+            depth_estimator: DepthEstimator,
+            point_picker: PointPicker,
+            object_tracker: ObjectTracker,
+            executor: concurrent.futures.Executor
+        ):
+        self.object_detector = object_detector
+        self.depth_estimator = depth_estimator
+        self.point_picker = point_picker
+        self.object_tracker = object_tracker
+        self.executor = executor
+
+    def __call__(self,
+            frame: NDArray[(Any, Any, 3), np.uint8],
+        ) -> List[ObjectDetection]:
+        future_detections = self.executor.submit(self.object_detector.__call__, frame)
+        future_depth_map = self.executor.submit(self.depth_estimator.__call__, frame)
+        detections = future_detections.result()
+        depth_map = future_depth_map.result()
         self.object_tracker(detections)
         # self.point_picker(detections, frame, depth_map)
         return detections
