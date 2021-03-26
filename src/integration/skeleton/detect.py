b923e9b7e0de4ed1e1f29e710d785b5270f91c6c
Jacob <jacobcohen76@protonmail.com>
2021-03-26 11:21:30 -0700

    fixed gpu config setting

--- src/integration/skeleton/detect.py
+++ src/integration/skeleton/detect.py
@@ -8,6 +8,10 @@ from cv2 import cv2
 import numpy as np
 import tensorflow as tf
 
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if physical_devices:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 from absl import app, flags, logging
 from absl.flags import FLAGS
 
@@ -21,10 +25,6 @@ from tensorflow.compat.v1 import InteractiveSession
 
 from typing import Any, Dict, Iterator, List, NamedTuple, Set, Tuple, TypeVar
 from nptyping import NDArray
-
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if physical_devices:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 ID = TypeVar('ID')
 
